79e519a2700765992f8efff4a8bbdd09ec930d65
Simon Buyck <simon.buyck@student.howest.be>
2020-06-23 09:10:49 +0200

    all matches can be viewed

--- src/components/Dances/Dances.js
+++ src/components/Dances/Dances.js
@@ -2,24 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 import style from "./Dances.module.css";
 import { useObserver } from "mobx-react-lite";
+import { useStore } from "../../hooks/useStore";
+import MatchPartner from "../MatchPartner/MatchPartner";
 
 const Profile = () => {
-    
-    return useObserver(() => (
-        <>
-        <header className={style.header}>
+  const { uiStore } = useStore();
+
+  return useObserver(() => (
+    <>
+      <header className={style.header}>
         <span></span>
         <h1 className={style.header__title}>Your Last Dances</h1>
         <span></span>
-        </header>
-        <main className={style.main + ' ' + style.main__footer}>
+      </header>
+      <main className={style.main + " " + style.main__footer}>
+        {uiStore.currentUser.matches.length !== 0 ? (
+          <section className={style.items}>
+            {uiStore.currentUser.matches.map((m) => (
+            <MatchPartner match={m} />
+            ))}
+          </section>
+        ) : (
+          <>
+            <p className={style.p}>You haven't done any dances yet.</p>
+            <Link to="/swipe" className={style.button}>
+              Find a Dancer
+            </Link>
+          </>
+        )}
         {
-                //if there are previous dances:
-            }
-            {
-            
-
-            /* <section className={style.items}>
+          //if there are previous dances:
+        }
+        {/* 
 
             //for each van last dances
                 <div className={style.item}>
@@ -32,14 +46,12 @@ const Profile = () => {
                 </div>
 
             </section> */}
-            {
-                //else:
-            }
-            <p className={style.p}>You haven't done any dances yet.</p>
-            <Link to="/swipe" className={style.button}>Find a Dancer</Link>
-        </main>
-        </>
-        ));
-    };
-    
-    export default Profile;
+        {
+          //else:
+        }
+      </main>
+    </>
+  ));
+};
+
+export default Profile;
